7b256ce1c1719af57e6aee527bbe0dec1d4fd766
akashre31 <116092315+akashre31@users.noreply.github.com>
2023-03-21 21:31:21 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,14 @@
-window.promises = [res(hi),res(hello),res(hey),res(namaste),res(amen)];
+window.promises = [ return new promise((res,rej)=>{
+	res(console.log("hi"))});
+				   return new promise((res,rej)=>{
+	res(console.log("hello"))});
+return new promise((res,rej)=>{
+	res(console.log("hey"))});
+return new promise((res,rej)=>{
+	res(console.log("namaste"))});
+return new promise((res,rej)=>{
+	res(console.log("namaskara"))});
+				   ];
 promises.any();
 // Do not change the code above this
 // add your promises to the array `promises`
