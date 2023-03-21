75d6a0d350a867f982e91c7ae8b0cbf9b5b9207e
akashre31 <116092315+akashre31@users.noreply.github.com>
2023-03-21 21:26:22 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-window.promises = [];
-
+window.promises = [res(hi),res(hello),res(hey),res(namaste),res(amen)];
+promises.any();
 // Do not change the code above this
 // add your promises to the array `promises`
